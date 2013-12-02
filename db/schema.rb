2f4a6f12b500edf45affc09c9b9e2a5c15374ead
Chip Eyler <ceyler@gmail.com>
2013-12-02 06:56:46 -0500

    generated pins scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131201121152) do
+ActiveRecord::Schema.define(version: 20131202112018) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
